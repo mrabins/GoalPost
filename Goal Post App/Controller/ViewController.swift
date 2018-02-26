d4a1682869daef7a2376670e2839f57c85d23a95
Mark Rabins <markrabins@gmail.com>
2018-02-25 21:53:17 -0800

    Set Up UI

--- Goal Post App/Controller/ViewController.swift
+++ Goal Post App/Controller/ViewController.swift
@@ -9,15 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var goalTableView: UITableView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addGoalButtonWasPressed(_ sender: Any) {
+        
+        print("button me")
+    
     }
 
 
