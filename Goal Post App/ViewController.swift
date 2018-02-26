a22331ba549d66ec224ef2e18e1cb2f465afa4b9
Mark Rabins <markrabins@gmail.com>
2018-02-25 21:24:17 -0800

    Initial Commit

---
+++ Goal Post App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Goal Post App
+//
+//  Created by Mark Rabins on 2/25/18.
+//  Copyright © 2018 Mark Rabins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
